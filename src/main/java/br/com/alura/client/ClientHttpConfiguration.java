8e06d44e9f9732c4052acccd443fda34644b2109
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-27 11:27:06 -0300

    renomeando pacote

--- src/main/java/br/com/alura/client/ClientHttpConfiguration.java
+++ src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.alura.Client;
+package br.com.alura.client;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -10,8 +10,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ClientHttpConfiguration {
-    public HttpResponse<String> dispararRequisicaoGet( String uri) throws IOException, InterruptedException {
+
+    public HttpResponse<String> dispararRequisicaoGet(String uri) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -19,13 +21,15 @@ public class ClientHttpConfiguration {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    public   HttpResponse<String> dispararRequisicaoPost( String uri, Object object) throws IOException, InterruptedException {
+    public HttpResponse<String> dispararRequisicaoPost(String uri, Object object) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
                 .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(object)))
                 .build();
+
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
