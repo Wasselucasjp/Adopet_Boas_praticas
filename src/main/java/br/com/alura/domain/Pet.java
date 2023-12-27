3ee80c7011afa6edaeea5e91a4324e938fe96b20
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-27 08:59:17 -0300

    refatorando classe pet

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,6 +1,7 @@
 package br.com.alura.domain;
 
 public class Pet {
+    private Long id;
     private String nome;
     private String tipo;
     private String raca;
@@ -21,47 +22,23 @@ public class Pet {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getTipo() {
         return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
     }
 
     public String getRaca() {
         return raca;
     }
 
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
-
     public String getCor() {
         return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
     }
 
     public Integer getIdade() {
         return idade;
     }
 
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public Float getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Float peso) {
-        this.peso = peso;
+    public long getId() {
+        return id;
     }
 }
