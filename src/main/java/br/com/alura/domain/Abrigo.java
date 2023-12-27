504f28a0323d9e6033860753a1038463ac88e82b
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-27 08:58:55 -0300

    refatorando classe abrigo

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -1,6 +1,7 @@
 package br.com.alura.domain;
 
 public class Abrigo {
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
@@ -11,27 +12,25 @@ public class Abrigo {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+
 
     public String getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
 }
